e59f80b1bf979cde9b47cba19b150a48aedb4446
Angel Gonzalez <58734248+MeBeAngel@users.noreply.github.com>
2022-05-22 19:45:59 -0400

    Animatable

--- components/DirectoryComponent.js
+++ components/DirectoryComponent.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { View, FlatList, Text } from 'react-native';
 import { ListItem } from 'react-native-elements';
 import { Tile } from 'react-native-elements';
+import * as Animatable from 'react-native-animatable';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
 import Loading from './LoadingComponent';
@@ -21,15 +22,17 @@ class Directory extends Component {
 
     render() {
         const { navigate } = this.props.navigation;
-        const renderDirectoryItem = ({item}) => {
+        const renderDirectoryItem = ({ item }) => {
             return (
-                <Tile
-                    title={item.name}
-                    caption={item.description}
-                    featured
-                    onPress={() => navigate('CampsiteInfo', { campsiteId: item.id })}
-                    imageSrc={{uri: baseUrl + item.image}}
-                />
+                <Animatable.View animation='fadeInRightBig' duration={2000}>
+                    <Tile
+                        title={item.name}
+                        caption={item.description}
+                        featured
+                        onPress={() => navigate('CampsiteInfo', { campsiteId: item.id })}
+                        imageSrc={{ uri: baseUrl + item.image }}
+                    />
+                </Animatable.View>
             );
         };
 
